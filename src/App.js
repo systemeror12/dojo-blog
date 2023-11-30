c19f41f46577a3225012f71b0ee2d5b4ac305f7e
systemeror12 <vrixenmendoza@gmail.com>
2023-11-30 19:19:41 +0800

    final edits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from './navbar';
 import Home from './Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,28 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    /*const [name, setName] = useState('mario')
+    const [age, setAge] = useState(25)
+    const handleClick = () => { 
+        setName('luigi');
+        setAge(30);
+    }
+    */
+
+    /*const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    */
+
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading....</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
